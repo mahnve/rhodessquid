521a7f38527271069b44f4dff6a5c79f704fda5f
Marcus Ahnve <marcus@ahnve.com>
2015-05-28 00:28:31 +0200

    New table structure

--- migrations/2015-03-26T1745.up.sql
+++ migrations/2015-03-26T1745.up.sql
@@ -1,2 +1,16 @@
-create extension if not exists hstore;
-create table phrases (id serial, key varchar(64) unique, translations hstore);
+CREATE TABLE lang_codes (
+        id serial PRIMARY KEY,
+        lang_code varchar(10) UNIQUE NOT NULL
+        );
+
+CREATE TABLE keys (
+        id serial PRIMARY KEY,
+        key_name varchar(64) NOT NULL
+        );
+
+CREATE TABLE phrases (
+        phrase varchar(512) NOT NULL,
+        lang_code_id integer NOT NULL REFERENCES lang_codes(id),
+        key_id integer NOT NULL REFERENCES keys(id),
+        PRIMARY KEY(lang_code_id, key_id)
+        );
