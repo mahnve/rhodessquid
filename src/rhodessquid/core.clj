e8680f7af168b6be3e1744e4c8be84383a6b5dc1
Marcus Ahnve <marcus@ahnve.com>
2015-05-31 01:12:56 +0200

    Destructuring ftw

--- src/rhodessquid/core.clj
+++ src/rhodessquid/core.clj
@@ -13,48 +13,37 @@
               :user "mahnve"})
 
 (defn translation-for
-  ([ctx]
-   (let [key (get-in ctx [:request :params :key])
-         lang (get-in ctx [:request :params :lang])]
-     (translation-for key lang)))
+  ([{{{key :key lang :lang} :params} :request}]
+   (translation-for key lang))
   ([key lang]
    {:phrase  (-> (db/find-phrase db-spec key lang) first :phrase)}))
 
-(defn with-extracted-params [ctx f]
-  (let [key (get-in ctx [:request :params :key])
-        lang (get-in ctx [:request :params :lang])
-        body(slurp (get-in ctx [:request :body]))]
-    (f key lang body)))
-
 (defn create-translation!
-  ([ctx]
-   (with-extracted-params ctx create-translation!))
   ([key lang phrase]
    (jdbc/with-db-transaction [connection db-spec]
-     (let [key
-           (:key_name (db/create-key<! connection key ))]
-       (db/create-phrase! connection key lang phrase)))))
+     (db/create-key<! connection key )
+     (db/create-phrase! connection key lang phrase))))
 
 (defn update-translation!
-  ([ctx]
-   (with-extracted-params ctx update-translation!))
   ([key lang phrase]
    (db/update-phrase! db-spec phrase key lang)))
 
-(defn write-translation! [ctx]
-  (if (:phrase ctx)
-    (update-translation! ctx)
-    (create-translation! ctx)))
+(defn write-translation!
+  [{{{key :key lang :lang} :params body :body } :request phrase :phrase}]
+  (let [body-data (slurp body)]
+    (if phrase
+      (update-translation! key lang body-data)
+      (create-translation! key lang body-data))))
 
 (defn list-phrases
   ([_] (list-phrases))
-  ([] (db/all-phrases db-spec)))
+  ([] (pr-str (db/all-phrases db-spec))))
 
 (defresource translation [key lang]
   :allowed-methods [:get :post :put]
   :available-media-types ["text/plain"]
   :exists? translation-for
-  :handle-ok (fn [ctx] (:phrase ctx))
+  :handle-ok (fn [{phrase :phrase}] phrase)
   :put! write-translation!
   :post! write-translation!
   :handle-not-found (str "Translation for key: '" key "', lang: '" lang "' not found"))
@@ -63,6 +52,7 @@
   :allowed-methods [:get]
   :available-media-types ["text/plain"]
   :handle-ok list-phrases)
+
 (defroutes app
   (ANY "/phrases/:key/:lang" [key lang]  (translation key lang))
   (ANY "/phrases" [] (translations)))
