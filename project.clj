668d7bed5eda8d364d9a45b95a6420111e9860b9
Marcus Ahnve <marcus@ahnve.com>
2015-09-21 23:37:21 +0200

    Big flytt

--- project.clj
+++ project.clj
@@ -6,8 +6,37 @@
   :plugins [[lein-ring "0.8.11"]
             [ragtime/ragtime.lein "0.3.8"]
             [cider/cider-nrepl "0.9.0-SNAPSHOT"]
-            [refactor-nrepl "1.1.0"]]
-  :ring {:handler rhodessquid.core/handler}
+            [refactor-nrepl "1.1.0"]
+            [lein-environ "1.0.0"]
+            [lein-asset-minifier "0.2.2"]]
+
+  :ring {:handler rhodessquid.core/handler
+         :uberwar-name "rhodessquid.war"}
+
+  :min-lein-version "2.5.0"
+  :uberjar-name "rhodessquid.jar"
+
+  :main rhodessquid.core
+
+  :clean-targets ^{:protect false} [:target-path
+                                    [:cljsbuild :builds :app :compiler :output-dir]
+                                    [:cljsbuild :builds :app :compiler :output-to]]
+
+
+  :source-paths ["src/clj" "src/cljc"]
+
+  :minify-assets
+  {:assets
+   {"resources/public/css/site.min.css" "resources/public/css/site.css"}}
+
+
+  :cljsbuild {:builds {:app {:source-paths ["src/cljs" "src/cljc"]
+                             :compiler {:output-to "resources/public/js/app.js"
+                                        :output-dir "resources/public/js/out"
+                                        :asset-path "js/out"
+                                        :optimizations :none
+                                        :pretty-print true}}}}
+
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [liberator "0.12.2"]
                  [compojure "1.3.2"]
@@ -20,7 +49,51 @@
                  [ragtime "0.3.8"]
                  [org.clojure/tools.nrepl "0.2.8"]
                  [postgresql/postgresql "8.4-702.jdbc4"]
-                 [expectations "2.0.9"]]
+                 [expectations "2.0.9"]
+                 [org.clojure/clojurescript "1.7.107" :scope "provided"]
+                 [secretary "1.2.3"]
+                 [cljsjs/react "0.13.3-1"]
+                 [prone "0.8.2"]
+                 [environ "1.0.0"]]
 
   :ragtime {:migrations ragtime.sql.files/migrations
-            :database "jdbc:postgresql://localhost:5432/rhodessquid"})
+            :database "jdbc:postgresql://localhost:5432/rhodessquid"}
+
+  :profiles {:dev {:repl-options {:init-ns rhodessquid.repl}
+                   :dependencies [[ring/ring-mock]
+                                  [ring/ring-devel]
+                                  [lein-figwheel "0.3.7"]
+                                  [org.clojure/tools.nrepl "0.2.10"]
+                                  [pjstadig/humane-test-output "0.7.0"]]
+
+                   :source-paths ["env/dev/clj"]
+
+                   :plugins [[lein-figwheel "0.3.7"]
+                             [lein-cljsbuild "1.0.6"]]
+
+                   :injections [(require 'pjstadig.humane-test-output)
+                                (pjstadig.humane-test-output/activate!)]
+
+                   :figwheel {:http-server-root "public"
+                              :server-port 3449
+                              :nrepl-port 7002
+                              :css-dirs ["resources/public.css"]
+                              :ring-handler rhodessquid.core/handler}
+
+                   :env {:dev true}
+
+                   :cljsbuild {:builds {:app {:source-paths ["env/dev/cljs"]
+                                              :compiler {:main "rhodessquid.dev"
+                                                         :source-map true}}
+                                        }
+                               }}
+             :uberjar {:hooks [leiningen.cljsbuild minify-assets.plugin/hooks]
+                       :env {:production true}
+                       :aot :all
+                       :omit-source true
+                       :cljsbuild {:jar true
+                                   :builds {:app
+                                            {:source-paths ["env/prod/cljs"]
+                                             :compiler
+                                             {:optimizations :advanced
+                                              :pretty-print false }}}}}})
